0553d3abcd00856dbd5e2aff1e63ffe23171e5c5
Sam Jett <samjett247@gmail.com>
2019-06-29 13:41:57 -0400

    I hate .DS_Store

--- interactivecrop/samples.py
+++ interactivecrop/samples.py
@@ -1,12 +1,8 @@
-import os
-
 from PIL import Image
 import numpy as np
 
-PATH_TO_SAMPLES = 'interactivecrop/image_samples'
+PATH_TO_SAMPLES = 'interactivecrop/image_samples/'
+SAMPLES = ['bird.jpg', 'castle.jpg', 'rhino.jpg', 'statue.jpg', 'tower.jpg']
 
-sample_images = [np.array(Image.open(PATH_TO_SAMPLES+'/'+im)) for im in os.listdir(PATH_TO_SAMPLES)] 
-sample_names = [i[:-4].title() for i in os.listdir(PATH_TO_SAMPLES)]
-
-if __name__=='__main__':
-    print(sample_images)
+sample_images = [np.array(Image.open(PATH_TO_SAMPLES+im)) for im in SAMPLES] 
+sample_names = [i[:-4].title() for i in SAMPLES]
