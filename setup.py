c9f3dc9455e02c95e2e8c0f1c60068f99ec0c965
Sam Jett <samjett247@gmail.com>
2019-06-29 13:30:59 -0400

    Include my image data plz

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
    long_description_content_type="text/markdown",
      url="https://github.com/samjett247/interactive_crop",
      packages=setuptools.find_packages(),
+     include_package_data=True,
      install_requires=[
         'pandas',
         'numpy',
