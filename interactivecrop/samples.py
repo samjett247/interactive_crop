968bc14c2a9bd0a3c04fa163c7977d5b41785698
Sam Jett <samjett247@gmail.com>
2019-06-30 07:41:53 -0500

    Tagging v9 and no changes planned soon

--- interactivecrop/samples.py
+++ interactivecrop/samples.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import numpy as np
+import os
 
-PATH_TO_SAMPLES = 'interactivecrop/image_samples/'
+dirname = os.path.dirname(__file__)
+PATH_TO_SAMPLES = os.path.join(dirname, 'image_samples/')
 SAMPLES = ['bird.jpg', 'castle.jpg', 'rhino.jpg', 'statue.jpg', 'tower.jpg']
 
 sample_images = [np.array(Image.open(PATH_TO_SAMPLES+im)) for im in SAMPLES] 
